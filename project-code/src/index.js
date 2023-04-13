45dec3d3b182aaf06c86bc8d252f018ecb21823a
Sean Carter <seca6364@colorado.edu>
2023-04-13 16:49:42 -0600

    Docker working now

---
+++ project-code/src/index.js
@@ -0,0 +1,10 @@
+const http = require("http"); 
+//create a server object: 
+http 
+    .createServer(function (req, res) { 
+    res.write("<h1>Hello World!</h1>"); 
+    //write a response to the client  
+    res.end();
+    //end the response 
+    }) .listen(3000); 
+//Server runs on localhost:3000 
