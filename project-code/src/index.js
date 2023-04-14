cb129872716d0cdb5330ad77fbe24dc62f2ee0e7
jessten0202 <jesus.carnero@colorado.edu>
2023-04-14 03:29:25 -0600

    Staged index.js to setup server and allow api calls through ejs

--- project-code/src/index.js
+++ project-code/src/index.js
@@ -1,10 +1,60 @@
-const http = require("http"); 
-//create a server object: 
-http 
-    .createServer(function (req, res) { 
-    res.write("<h1>Hello World!</h1>"); 
-    //write a response to the client  
-    res.end();
-    //end the response 
-    }) .listen(3000); 
-//Server runs on localhost:3000 
+// Some code taken from previous lab
+
+const express = require('express'); // To build an application server or API
+const app = express();
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+
+// database configuration
+const dbConfig = {
+    host: 'db', // the database server
+    port: 5432, // the database port
+    database: process.env.POSTGRES_DB, // the database name
+    user: process.env.POSTGRES_USER, // the user account to connect with
+    password: process.env.POSTGRES_PASSWORD, // the password of the user account
+  };
+  
+  const db = pgp(dbConfig);
+  // test your database
+  db.connect()
+    .then(obj => {
+      console.log('Database connection successful'); // you can view this message in the docker compose logs
+      obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+      console.log('ERROR:', error.message || error);
+    });
+
+
+app.set('view engine', 'ejs'); // set the view engine to EJS
+app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+
+// initialize session variables
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// Api to render login and register pages
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+})
+
+
+// Start server
+app.listen(3000);
+console.log('Server is listening on port 3000');
