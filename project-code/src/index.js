4f48aa8981444ad7bcbd63f963786c95282d51d9
jessten0202 <jesus.carnero@colorado.edu>
2023-05-03 21:15:54 -0600

    Fixed posting and comments with 0 input

--- project-code/src/index.js
+++ project-code/src/index.js
@@ -139,7 +139,7 @@ app.post('/add_comment/:post_id/:chain', function (req, res) {
   var topic1 = req.params.post_id;
   var chain1 = req.params.chain; //Is currently a INT, though will be used for nesting if we ever get there.
   var comm1 = req.body.body;
-  if (topic1 != null && comm1 != null){
+  if (topic1 != '' && comm1 != ''){
     const query = `insert into comments (comment_id, post_id, user_id, chain, body) values ((SELECT max(comment_id)+1 FROM comments),'${topic1}','${req.session.user.user_id}','${chain1}', '${comm1}')  returning * ;`;//    const query = `insert into comments (post_id, user_id, chain, body) values ('${topic1}','${req.session.user.user_id}', '${chain1}','${comm1}')  returning * ;`;
     db.any(query, [
       req.params.post_id,
@@ -166,9 +166,7 @@ app.post('/add_comment/:post_id/:chain', function (req, res) {
   }
 
   else{
-    res.render('pages/home',{
-    message: "Topic Title or Body Was Empty, Comment Not Posted",
-    }); 
+    res.redirect('/comments/'+topic1); 
   }
 });
 
@@ -402,6 +400,7 @@ app.post('/add_post', function (req, res) {
     console.log("Activated else block.");
     res.redirect('/home'); 
   }
+  res.redirect('/home')
 });
 
 
